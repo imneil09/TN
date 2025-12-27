c011fe5b95dc2e6975f9ac8855aeb6bcd0db4d62
ALPHABETS LAB <ainnovations20@gmail.com>
2025-12-27 19:26:39 +0530

    start

--- tripuranow/settings.py
+++ tripuranow/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
-    {
+        {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
     {
@@ -105,7 +105,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+9
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
